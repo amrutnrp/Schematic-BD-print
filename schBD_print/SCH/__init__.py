ff730aea847d93c582c7f669691cfae5e2c4d63b
Amrutnarayan Panigrahi (Amrut) <amrutnarayan@gmail.com>
2024-10-22 14:31:58 +0530

    added one more import

--- schBD_print/SCH/__init__.py
+++ schBD_print/SCH/__init__.py
@@ -1,3 +1,3 @@
 from .SCH_processor import SCH_processor
 from .Debug_functions import view_str, shape, validate_rect
-from .string2D_functions import str_2D, build_lines
+from .string2D_functions import str_2D, build_lines, glue_string
--- schBD_print/__init__.py
+++ schBD_print/__init__.py
@@ -1,3 +1,3 @@
 from .SCH import SCH_processor
 from .Util import make_web_page_nOpen
-from .SCH import view_str, shape, validate_rect, str_2D, build_lines
+from .SCH import view_str, shape, validate_rect, str_2D, build_lines, glue_string
