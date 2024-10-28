192955b27fdda496ffae1f9ca59405fdd61d6105
Amrutnarayan Panigrahi (Amrut) <amrutnarayan@gmail.com>
2024-10-28 18:34:16 +0530

    updated tests

--- Test2.py
+++ Test2.py
@@ -3,12 +3,13 @@ from schBD_print import *
 # from imager import improve_view, view_str
 
 
+
 if __name__ == "__main__":
 
     import pickle
 
-    import sys
-    _this = sys.modules[__name__]
+    # import sys
+    # _this = sys.modules[__name__]
     with open('s.pkl', 'rb') as f:
         S1, S2, S3 = pickle.load(f)
 
@@ -18,3 +19,4 @@ if __name__ == "__main__":
 
     A= improve_view(S2)
     view_str(A)
+#
